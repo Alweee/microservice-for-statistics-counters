a85e1cf0e0591f4f62618f6a93d073f32bec699c
Sasha <sasha95296@mail.ru>
2022-08-13 10:24:58 +0300

    4th commit

--- statistics_counter/api/views.py
+++ statistics_counter/api/views.py
@@ -1,14 +1,24 @@
 from rest_framework import filters
-from django_filters import DateRangeFilter, DateFilter # может быть поможет эта библиотека в моём таске, разобраться.
+from django_filters import rest_framework as rest_filters
 
 from api.mixins import CreateRetrieveDestroyViewSet
 from api.models import Statistics
 from api.serializers import StatisticsSerializer
 
 
+class DateFilter(rest_filters.FilterSet):
+    date = rest_filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Statistics
+        fields = ('date',)
+
+
 class StatisticsViewSet(CreateRetrieveDestroyViewSet):
     queryset = Statistics.objects.all()
     serializer_class = StatisticsSerializer
-    # filterset_class такая штука ещё есть
-    filter_backends = (filters.OrderingFilter,)
+    filterset_class = DateFilter
+    filter_backends = (filters.OrderingFilter,
+                       rest_filters.DjangoFilterBackend,)
     ordering_fields = '__all__'
+    ordering = ('date',)
--- statistics_counter/statistics_counter/settings.py
+++ statistics_counter/statistics_counter/settings.py
@@ -50,13 +50,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'statistics_counter.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
